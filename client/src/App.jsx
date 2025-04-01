111b3bd1184311d336b6205d2bb00b79bc581706
ramezmezni16 <ramez.nabila@gmail.com>
2025-04-02 00:47:00 +0100

    the init commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-900">
+      <h1 className="text-4xl font-bold text-white">Hello World !</h1>
+    </div>
+  )
+}
+
+export default App
