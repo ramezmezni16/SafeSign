1581d870e60a7ce4d403b34371330c98f8b474c3
ramezmezni16 <ramez.nabila@gmail.com>
2025-06-17 15:52:45 +0100

    commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const cors = require("cors")
+const multer = require("multer")
+require("dotenv").config()
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+// Middlewares
+app.use(cors())
+app.use(express.json())
+
+// Import routes
+const contractRoutes = require("./routes/contract.routes")
+app.use("/api", contractRoutes)
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
